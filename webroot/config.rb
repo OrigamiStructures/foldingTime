30f0a8a615672348a01538cfd3f7cb77da142123
Jason Tempestini <jason@curlymedia.com>
2015-09-28 15:14:23 -0700

    redone Foundation...

--- webroot/config.rb
+++ webroot/config.rb
@@ -3,9 +3,9 @@ add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = "stylesheets"
 sass_dir = "scss"
-images_dir = "img"
+images_dir = "images"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
